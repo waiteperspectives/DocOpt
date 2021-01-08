0fecdaeca43fefd4c9a1a158d5d501971baba50b
Zach Waite <zach@waiteperspectives.com>
2021-01-08 13:01:24 -0500

    generalized

--- DocOpt.Demo/Program.fs
+++ DocOpt.Demo/Program.fs
@@ -1,31 +1,158 @@
-open Docopt
+// desired behavior
+// cli inputs |> ingest |> handle or query |> publish
+// where ingest =
+// cli inputs |> optdict |> Stop DTO |> DTO |> Command or Query
+// and handle =
+// Command |> commandhandler |> events |> tee "some file" |> eventhandlers |> "ack"
+// and query =
+// Query |> queryhandler |> "query results string"
+// and publish =
+// "ack" or "query results string" |> printfn "%s"
 
-let doc = """
-Naval Fate.
+
+open Docopt
+open Docopt.Arguments
+
+
+let DOC = """
+Herd Inventory
+=============================================================
 
 Usage:
-  naval_fate.exe ship new <name>...
-  naval_fate.exe ship <name> move <x> <y> [--speed=<kn>]
-  naval_fate.exe ship shoot <x> <y>
-  naval_fate.exe mine (set|remove) <x> <y> [--moored | --drifting]
-  naval_fate.exe (-h | --help)
-  naval_fate.exe --version
+  cow born <name> --dam=<dam> [--asof=<date>] [--force]...
+  cow died <name> [--asof=<date>] [--force]...
+  cow bought <name> [--asof=<date>] [--force]...
+  cow sold <name> [--asof=<date>]...
+  cow show <name> [--asof=<date>]...
+  cow show --report=<report> [--asof=<date>]
+  cow (-h | --help)
+  cow --version
+
 
 Options:
   -h --help     Show this screen.
   --version     Show version.
-  --speed=<kn>  Speed in knots [default: 10].
-  --moored      Moored (anchored) mine.
-  --drifting    Drifting mine.
 """
+
+let VERSION = "\nHerd Inventory - Version 1.0.0\n\n"
+let NOTIMPLEMENTED = "This command has not yet been implmented"
+let INVALIDCOMMAND = sprintf "Invalid Command!\n%s" DOC
+
+type BornCommand = {
+  name: string
+  dam: string
+}
+
+// "Help" and "Version" are not domain objects, so should not be sent deeper
+// hence calling this CliCommand. There should probably be another thing DomainCommand
+type CliCommand =
+  | Help of string
+  | Version of string
+  | Invalid of string
+  | Born of BornCommand
+
+type CliResponse = CliResponse of string
+
+let printCliResponse (response:CliResponse): unit =
+  match response with
+  | CliResponse s -> printfn "%s" s
+
+// This is the interface with the domain.
+// For "help" and "version" cases, just repackage the input
+// But for born, process the domain command, then wrap the result in CliResponse
+let handleCliCommand (request:CliCommand): CliResponse =
+  match request with
+  | Help s -> CliResponse s
+  | Version s -> CliResponse s
+  | Born r -> CliResponse (sprintf "{name=%s; dam=%s}" r.name r.dam)
+  | Invalid x -> CliResponse x
+
+
+type StopOrContinue =
+  | Stop of CliCommand
+  | Continue of Docopt.Arguments.Dictionary
+
+
+let bind continueHandler stopOrContinue =
+  match stopOrContinue with
+  | Continue parsed -> continueHandler parsed
+  | Stop value -> Stop value
+
+
+let makeBornCommand (parsed:Docopt.Arguments.Dictionary) : CliCommand =
+  let herName = 
+    match parsed.Item "<name>" with
+    | Argument s -> s
+    | _ -> "who cares"
+
+  let herDam = 
+    match parsed.Item "--dam" with
+    | Argument s -> s
+    | _ -> "who cares"
+  
+  Born {name=herName; dam=herDam}
+  
+
+
+// switch functions to extract a subcommand and either:
+// 1) stop parsing and handle the subcommand
+// 2) continue parsing
+// TODO: This can be generalized
+let stopMaker k f parsed =
+  let wrapped (parsed:Docopt.Arguments.Dictionary): StopOrContinue =
+    let rs = parsed.Item k
+    match rs with
+    | Flag _ -> Stop (f parsed)
+    | None -> Continue parsed
+    | _ -> failwith "????"
+  wrapped parsed
+
+let helpStop (parsed:Docopt.Arguments.Dictionary): StopOrContinue =
+  let maybeHelp = parsed.Item "-h"
+  match maybeHelp with
+  | Flag _ -> Stop (Help DOC)
+  | None -> Continue parsed
+  | _ -> failwith "????"
+
+
+let versionStop (parsed:Docopt.Arguments.Dictionary): StopOrContinue =
+  let maybeVersion = parsed.Item "--version"
+  match maybeVersion with
+  | Flag _ -> Stop (Version VERSION)
+  | None -> Continue parsed
+  | _ -> failwith "????"
+
+
+let bornStop (parsed:Docopt.Arguments.Dictionary): StopOrContinue =
+  let maybeBorn = parsed.Item "born"
+  match maybeBorn with
+  | Flag _ -> Stop (makeBornCommand parsed)
+  | None -> Continue parsed
+  | _ -> failwith "????"
+
+
+
+// wire up railroad (help | version | born | died | bought | sold ...)
+// uses Railway oriented programing style of handling subcommands -
+// if one is matched, parsing stops and executes the subcommand
+let versionStop' = bind versionStop
+let bornStop' = bind bornStop
+let convertToCliCommand (input:Docopt.Arguments.Dictionary): CliCommand =
+  let railroad = helpStop >> versionStop' >> bornStop'
+  input |> railroad |> (fun stopOrContinue ->
+    match stopOrContinue with
+    | Stop cliCmd -> cliCmd
+    | Continue _ -> Invalid INVALIDCOMMAND)
+
 
 [<EntryPoint>]
 let main argv =
-  let docopt = Docopt(doc)
   try
-    let dict = docopt.Parse(argv)
-    printfn "Success:\n%A" dict
+    Docopt(DOC).Parse(argv)
+    |> convertToCliCommand
+    |> handleCliCommand
+    |> printCliResponse
     0
   with ArgvException(message) ->
     printfn "Error: %s" message
-    -42
+    -1
